568cedfb244d017c97f4ef20199dcff012b6f820
Simon Bohnen <simon.bohnen@tum.de>
2025-06-30 20:18:59 +0200

    exclude problematic files

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': ['old/**/*'],
+    },
+  },
   rewrites: async () => {
     return [
       {
